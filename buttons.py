12f741f702b0b514c7a432de5b98f83efceb488e
Inkogni7o <ezykov25@gmail.com>
2023-01-04 13:49:14 +0300

    rework button class

--- buttons.py
+++ buttons.py
@@ -2,37 +2,35 @@ import pygame
 
 
 class Button:
-    def __init__(self, screen: pygame.display, text: str, color: str, x: int, y: int, width: int, height: int):
+    def __init__(self, text: str, color1: str, color2: str,
+                 x: int, y: int, width: int, height: int):
         """
         Класс кнопки на экране
-        :param screen: экран, на котором отрисовывается кнопка
         :param text: текст внутри кнопки
-        :param color: цвет, задающийся словом (напр. yellow)
+        :param color1: цвет, задающийся словом (напр. yellow), ryj
         :param x: X координата вернего левого края кнопки
         :param y: Y координата вернего левого края кнопки
         :param width: ширина кнопки в пикселях
         :param height: высота кнопки в пикселях
         """
-        self.screen = screen
         self.text_btn = text
-        self.color = pygame.Color(color)
+        self.color1 = pygame.Color(color1)
+        self.color2 = pygame.Color(color2)
         self.x, self.y = x, y
         self.width, self.height = width, height
 
-    def draw(self, screen, outline=None):
+    def draw(self, screen: pygame.display, pos_x: int, pos_y: int, outline=None):
         if outline:
+            # отрисовка границы кнопки, если таковая имеется
             pygame.draw.rect(screen, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
 
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), 0)
-
-        if self.text_btn != '':
-            font = pygame.font.SysFont('comicsans', 60)
-            text = font.render(self.text_btn, True, (0, 0, 0))
-            screen.blit(text,
-                        (self.x + (self.width / 2 - text.get_width() / 2),
-                         self.y + (self.height / 2 - text.get_height() / 2)))
-
-    def update(self, pos_x: int, pos_y: int):
         if self.x <= pos_x <= self.x + self.width and self.y <= pos_y <= self.y + self.height:
-            # TODO: изменить вид кнопки при наведении на неё курсора
-            pass
+            pygame.draw.rect(screen, self.color2, (self.x, self.y, self.width, self.height), 0)
+        else:
+            pygame.draw.rect(screen, self.color1, (self.x, self.y, self.width, self.height), 0)
+
+        font = pygame.font.SysFont('comicsans', 60)
+        text = font.render(self.text_btn, True, (0, 0, 0))
+        screen.blit(text,
+                    (self.x + (self.width / 2 - text.get_width() / 2),
+                     self.y + (self.height / 2 - text.get_height() / 2)))
--- menu.py
+++ menu.py
@@ -1,13 +1,21 @@
 import pygame
 from sys import exit
 
+from buttons import Button
 
-def menu(screen: pygame.display, clock: pygame.time.Clock):
+
+def introductory_menu(screen: pygame.display, clock: pygame.time.Clock):
+    buttons = list()
+    buttons.append(Button('Начать игру!', 'green', 'red', 1450, 700, 400, 100))
+    buttons.append(Button('Настройки', 'green', 'red', 1450, 850, 400, 100))
+    buttons.append(Button('Выйти', 'green', 'red', 1450, 1000, 400, 100))
     while True:
+        screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-        screen.fill((0, 0, 0))
+        for button in buttons:
+            button.draw(screen, *pygame.mouse.get_pos())
         pygame.display.flip()
         clock.tick(60)
