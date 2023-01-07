7c7d93565d7faedf4633ac7212c2f2904610c796
Inkogni7o <ezykov25@gmail.com>
2023-01-07 20:01:44 +0300

    add textures of lvl

---
+++ src/textures/wall_textures.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="wall_textures" tilewidth="32" tileheight="32" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="sand.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="water_dark_blue.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="bold1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="bold2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="bold3.png"/>
+ </tile>
+</tileset>
